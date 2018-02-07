5d4ee682219e68b99ac1d537656e6b484aea588d
Madison Kalivoda <mkalivoda07@gmail.com>
2018-02-07 09:52:20 -0700

    adjusting command prompts

--- letter.js
+++ letter.js
@@ -1,21 +1,20 @@
-
 function Letter(letter) {
-    
-        this.letter = letter;
-    
-        if (this.letter == ',') {
-                this.display = ','
-        }else if (this.letter == "'"){
-            this.display = "'"
-        }else if (this.letter == '.'){
-            this.display = '.'
-        }else if (this.letter == '-'){
-            this.display = '-'
-        }else if (this.letter == ' '){
-            this.display = ' '
-        }else {
-            this.display = '_'
-        }	
-    }
-    
-    module.exports = Letter;
+
+	this.letter = letter;
+
+	if (this.letter == ',') {
+			this.display = ','
+	}else if (this.letter == "'"){
+		this.display = "'"
+	}else if (this.letter == '.'){
+		this.display = '.'
+	}else if (this.letter == '-'){
+		this.display = '-'
+	}else if (this.letter == ' '){
+		this.display = ' '
+	}else {
+		this.display = '_'
+	}	
+}
+
+module.exports = Letter;
